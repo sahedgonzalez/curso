6efe8408e5aadcdbfea69dfe00ce4555a44ab8d8
sahedgonzalez <73143335+sahedgonzalez@users.noreply.github.com>
2020-11-06 17:06:05 -0600

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using WebApplicationM3.Contexts;
+using WebApplicationM3.Entities;
+using WebApplicationM3.Models;
+
+namespace WebApplicationM3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(Configuration =>   // esto se hace para el mapeo de dto con sus clases y no hacerlo uno por uno
+                {
+                    Configuration.CreateMap<Autor, AutorDTO>();
+                    Configuration.CreateMap<Libro, LibroDTO>();
+                    Configuration.CreateMap<AutorCreacionDTO, Autor>().ReverseMap();
+                }, typeof(Startup));
+            services.AddResponseCaching();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnectionString"))); //se agrega pa la conexion a bd
+            services.AddControllers().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+                
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage(); 
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
